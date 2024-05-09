8d13c06887f48f9ea199e369e7531533ad48bccf
fdwqguhij <gs1flopezdj@hotmail.com>
2024-05-09 14:02:29 +0700

    feat

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const number = 10;
-if (number % 2 === 0) {
-  console.log("Even number");
-} else {
-  console.log("Odd number");
-}
+// Fund Alice and sponsor accounts
+await aptos.fundAccount({
+  accountAddress: aliceAddress,
+  amount: ALICE_INITIAL_BALANCE,
+});
+await aptos.fundAccount({
+  accountAddress: sponsorAddress,
+  amount: SPONSOR_INITIAL_BALANCE,
+});
+
+// Show account balances
+const aliceBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: aliceAddress,
+});
+const sponsorBalanceBefore = await aptos.getAccountCoinsData({
+  accountAddress: sponsorAddress,
+});
